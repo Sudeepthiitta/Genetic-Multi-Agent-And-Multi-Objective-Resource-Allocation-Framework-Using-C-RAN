6910950996e47f586681ae84314fb1cec8c5fbfc
Sudeepthiitta <148898455+Sudeepthiitta@users.noreply.github.com>
2026-06-27 22:56:44 +0530

    Update views.py

--- AllocationApp/views.py
+++ AllocationApp/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import render
 
+def home(request):
+    return render(request, "index.html")
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
